b1730bf3f4100ee6606ef5c57065a32106298b54
Nacho Verdon <nverblaz@gmail.com>
2020-08-16 09:54:46 +0200

    Declare variable out of for loop

--- replay_watcher/replay_watcher.v
+++ replay_watcher/replay_watcher.v
@@ -49,9 +49,10 @@ fn get_latest_slp_file(cfg Config) ?string {
 	slp_files := os.walk_ext(cfg.directory, slp_ext)
 	mut latest_slp_file := ""
 	mut latest_modified := 0
+	mut last_modified := 0
 
 	for file_name in slp_files {
-		last_modified := os.file_last_mod_unix(file_name)
+		last_modified = os.file_last_mod_unix(file_name)
 
 		if last_modified > latest_modified {
 			latest_modified = last_modified
